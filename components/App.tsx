e3802d5cb6efe482b31fe5282d99f68e6f2b57e6
xueyong <1085523029@qq.com>
2022-08-27 10:59:50 +0800

    :sparkles: BufferGeometry, Texture, Line

--- components/App.tsx
+++ components/App.tsx
@@ -10,15 +10,23 @@ import {
   useState,
 } from "react"
 import {
+  BufferAttribute,
+  BufferGeometry,
   CameraHelper,
   DirectionalLight,
   DirectionalLightHelper,
+  Float32BufferAttribute,
+  Line,
+  LineBasicMaterial,
   MathUtils,
   Mesh,
   PerspectiveCamera as PerspectiveCameraImpl,
+  TextureLoader,
 } from "three"
 import gsap from "gsap"
 import useShadowHelper from "./hooks/useShadowHelper"
+
+const loader = new TextureLoader()
 
 const context = createContext<{ control: any }>({
   control: null,
@@ -70,7 +78,7 @@ const Box = () => {
       }}
       castShadow
     >
-      <meshStandardMaterial color="#28bea0"></meshStandardMaterial>
+      <meshStandardMaterial map={loader.load("/tater.jpeg")}></meshStandardMaterial>
       <boxGeometry args={[1, 1, 1]}></boxGeometry>
       <Edges color="gray" scale={1.1} threshold={15} />
       <Cone position={[-4, 0, 0]} />
@@ -80,7 +88,7 @@ const Box = () => {
 
 const Sphere = () => {
   return (
-    <mesh position={[-2, 0, 0]} castShadow>
+    <mesh position={[-6, 0, 0]} castShadow>
       <meshNormalMaterial wireframe></meshNormalMaterial>
       <sphereGeometry args={[3, 100, 100]}></sphereGeometry>
       <Edges color="gray" scale={1.1} threshold={15} />
@@ -155,6 +163,29 @@ const useCameraControl = (controlRef: MutableRefObject<any>) => {
   }, [])
 }
 
+const Linee = () => {
+  const state = useThree()
+  useEffect(() => {
+    const geometry = new BufferGeometry()
+    const material = new LineBasicMaterial({ vertexColors: true })
+    const positions = [-2, 1, 0, 2, 2, 0, 2, 6, 1]
+    geometry.setAttribute("position", new Float32BufferAttribute(positions, 3))
+    geometry.setAttribute(
+      "color",
+      new BufferAttribute(new Float32Array([0.1, 0.2, 0.9, 1, 0.1, 0.1, 0, 1, 0.5, 0.2, 0.1, 1]), 4)
+    )
+    // const morphTarget = new Float32BufferAttribute([-2, 1, 0, 2, 2, 0, 2, 6, 1], 3)
+    // morphTarget.name = "target1"
+
+    // geometry.morphAttributes.position = [morphTarget]
+    // geometry.computeBoundingSphere()
+
+    const line = new Line(geometry, material)
+    state.scene.add(line)
+  })
+  return <></>
+}
+
 const Root = () => {
   const scene = useThree((state) => state.scene)
   const controlRef = useRef<any>()
@@ -184,6 +215,7 @@ const Root = () => {
       <axesHelper args={[4]}></axesHelper>
       <Box />
       <Sphere />
+      <Linee />
       <Floor />
     </context.Provider>
   )
@@ -196,6 +228,7 @@ type WrapperProps = {
 const Wrapper = (props: WrapperProps) => {
   return (
     <Canvas camera={{ position: [0, 0, 10] }} gl={{ antialias: true }} shadows>
+      <fog attach="fog" color="hotpink" near={1} far={50} />
       {props.children}
     </Canvas>
   )
